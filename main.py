eefa1f413c18683657718be1d5e99923f9d85914
donblizy <nbliziotis@icloud.com>
2023-01-08 16:37:55 +0000

    flapping animation and bird drawing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import pygame
 from pygame.locals import *
 
@@ -10,25 +11,64 @@ screen_width = 864
 screen_height = 936
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Flappy Bird")
+pygame.display.set_caption('Flappy Bird')
+
 
 # define game variables
 ground_scroll = 0
 scroll_speed = 4
 
-# load images etc.
-bg = pygame.image.load("assets/environment/bg.png")
-ground = pygame.image.load("assets/environment/ground.png")
+# load images
+bg = pygame.image.load('assets/environment/bg.png')
+ground_img = pygame.image.load('assets/environment/ground.png')
+
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'assets/character/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        # handle the flap animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
 
 run = True
-
 while run:
+
     clock.tick(fps)
+
     # draw background
     screen.blit(bg, (0, 0))
 
-    # draw and scroll ground image
-    screen.blit(ground, (ground_scroll, 768))
+    bird_group.draw(screen)
+    bird_group.update()
+
+    # draw and scroll the ground under background
+    screen.blit(ground_img, (ground_scroll, 768))
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
         ground_scroll = 0
@@ -38,6 +78,5 @@ while run:
             run = False
 
     pygame.display.update()
-
 
 pygame.quit()
